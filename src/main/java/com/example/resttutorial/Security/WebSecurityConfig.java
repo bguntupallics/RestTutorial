2b58c68b96aa0057c80b79c9746622961593267d
bguntupalli <bharguntupalli@gmail.com>
2023-06-30 14:18:15 -0400

    fixed cors issue

--- src/main/java/com/example/resttutorial/Security/WebSecurityConfig.java
+++ src/main/java/com/example/resttutorial/Security/WebSecurityConfig.java
@@ -6,13 +6,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests().requestMatchers("/api/employees").permitAll();
+        http
+                .authorizeHttpRequests().requestMatchers("/api/employees").permitAll()
+                .and()
+                .authorizeHttpRequests().anyRequest().authenticated();
+
 
         return http.build();
     }
