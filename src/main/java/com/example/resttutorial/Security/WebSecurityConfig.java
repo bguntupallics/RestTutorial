363da10fed615ff62feb1047d187bdf295de3d39
bguntupalli <bharguntupalli@gmail.com>
2023-07-05 19:50:54 -0400

    api token works

--- src/main/java/com/example/resttutorial/Security/WebSecurityConfig.java
+++ src/main/java/com/example/resttutorial/Security/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
@@ -15,10 +16,13 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests().requestMatchers("/api/employees").permitAll()
+                .authorizeHttpRequests().requestMatchers("/**").permitAll()
                 .and()
-                .authorizeHttpRequests().anyRequest().authenticated();
-
+                .authorizeHttpRequests().anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         return http.build();
     }
